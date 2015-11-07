7d2086b5eedc80974f3faef6cea3fdc56444b496
Radhika Parthasarathy <radhika@nextdoor.com>
2015-11-07 15:29:52 -0800

    neighborhood plotter

--- app/views/zipcode.py
+++ app/views/zipcode.py
@@ -2,7 +2,8 @@ __author__ = 'rganesh'
 
 # Import flask dependencies
 from flask import Blueprint, request, render_template
-
+from app.lib.shape_handlers import jsonmap
+import json
 # Define the blueprint:
 zipcode = Blueprint('zipcode', __name__, url_prefix='/zipcode')
 
@@ -10,7 +11,7 @@ zipcode = Blueprint('zipcode', __name__, url_prefix='/zipcode')
 @zipcode.route('/')
 @zipcode.route('/<zip_id>')
 def hello(zip_id=None):
-    return render_template('zipcode/zipcode.html', zipcode=get_zipcode_from_id(zip_id) if zip_id else None)
+    return render_template('zipcode/zipcode.html')
 
 
 def get_zipcode_from_id(zip_id):
@@ -18,3 +19,10 @@ def get_zipcode_from_id(zip_id):
                '2': 94115,
                '3': 95134}
     return zip_map[zip_id]
+
+@zipcode.route('/shpdata')
+def shapedata():
+    json_map = jsonmap()
+    return json.dumps(json_map)
+
+
